81fed673bcd3095fcf51d9880d6de52c41ab0868
Mariem <mariem.ehab234@gmail.com>
2024-06-06 12:35:50 +0000

    Task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -4,4 +4,15 @@
 
 def canUnlockAll(boxes):
     '''method that determines if all the boxes can be opened.'''
-    pass
+    openedSet = set()
+    Unlock(boxes, openedSet, 0)
+    return len(boxes) == len(openedSet)
+
+
+def Unlock(boxes, openedSet, index):
+    '''return a set of all unlocked boxes'''
+    if index not in openedSet:
+        openedSet.add(index)
+        for key in boxes[index]:
+            if key < len(boxes):
+                Unlock(boxes, openedSet, key)
