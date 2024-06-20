d3dd2a2c7c4f95af7361c8fca7cc1936f85c6c7c
Mariem <mariem.ehab234@gmail.com>
2024-06-20 06:23:06 -0700

    Task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 '''0. Log parsing'''
 import sys
 import re
-import random
 
 
 regex = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (200|301|400|401|403|404|405|500) (\d{1,4})'  # nopep8
@@ -13,12 +12,11 @@ if __name__ == '__main__':
     output = []
     try:
         for line in sys.stdin:
-            if (i % 10 == 0 and i != 0) or i > 10:
+            if i % 10 == 0 and i != 0:
                 print('File size: {}'.format(tot_file_size))
                 output_sorted = sorted(output, key=lambda x: x['status'])
                 for x in output_sorted:
                     print('{}: {}'.format(x['status'], x['num']))
-                i = 0
 
             match = re.match(regex, line.rstrip())
             if match:
@@ -34,7 +32,7 @@ if __name__ == '__main__':
                     output.append({'status': status_code, 'num': 1})
 
                 tot_file_size += file_size
-            i += 1
+                i += 1
     finally:
         print('File size: {}'.format(tot_file_size))
         output_sorted = sorted(output, key=lambda x: x['status'])
