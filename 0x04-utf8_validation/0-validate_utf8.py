13dde1bb7cd6aa04ea778f7aa183524b83c94a92
Mariem <mariem.ehab234@gmail.com>
2024-07-25 12:15:46 +0000

    Task 0

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -5,7 +5,24 @@
 def validUTF8(data):
     """method that determines if a given data set represents
        a valid UTF-8 encoding"""
-    for i in range(len(data)):
-        if data[i] > 255 or data[i] < 0:
-            return False
+
+    def countOnes(n):
+        count = 0
+        for i in range(7, -1, -1):
+            if n & (1 << i):
+                count += 1
+            else:
+                break
+        return count
+
+    bytes_count = 0
+    for i in data:
+        if bytes_count == 0:
+            bytes_count = countOnes(i)
+            if bytes_count > 4 or bytes_count == 1:
+                return False
+        else:
+            if countOnes(i) != 1:
+                return False
+            bytes_count -= 1
     return True
